29009b717bb37eb6ab90527bab319c5dd5b48a4b
megha_bolegaon <msbolegaon@gmail.com>
2026-02-09 02:05:28 +0530

    Update number_palidrome.py

--- string/number_palidrome.py
+++ string/number_palidrome.py
@@ -1,1 +1,18 @@
+# LeetCode 9 - Palindrome Number
+# Difficulty: Easy
+# Topic: Math
+# Approach: Reverse the number
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+def isPalindrome(x):
+    if x < 0:
+        return False
+    
+    original = x
+    rev = 0
 
+    while x > 0:
+        x,r =divmod(x,10)
+        rev = rev * 10 + r
+      
+    return original == rev
